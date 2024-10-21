ed66c83704bf2bfc9460ad800f2772d9f845839d
sagarlaxmanpanchal <panchalsagar2149@gmail.com>
2024-10-21 15:09:05 +0530

    changes

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        System.out.println("Hi this is sagarsssgit ");
+        System.out.println("Hi this is sagarsssgit ");
+    }
+    public void getRequest(){
+        System.out.println("get");
+    }
+}
